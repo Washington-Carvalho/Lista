c5bb0a702261bd8e8879e636602d9b73fa074c59
Washingtoncip <washingtoncip@outlook.com>
2026-03-12 22:15:01 -0300

    Exercicio 1

---
+++ Lista/Ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome: ");
+            String nome = Console.ReadLine();
+            Console.WriteLine("Olá, " +nome);
+        }
+    }
+}
