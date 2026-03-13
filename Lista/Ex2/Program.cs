188a770019e12a370be0b258a1681d74f425dbdf
Washingtoncip <washingtoncip@outlook.com>
2026-03-12 22:21:56 -0300

    Exercicio 2

---
+++ Lista/Ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Console.Write("Digite um numero: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite outro numero: ");
+            n2 = int.Parse(Console.ReadLine());
+            n1 = n1 + n2;
+            Console.WriteLine("Total: {0} ", n1);
+        }
+    }
+}
