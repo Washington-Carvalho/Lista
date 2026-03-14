3619a3abb3c1d4d730312ec45a8122d489df6c38
Washingtoncip <washingtoncip@outlook.com>
2026-03-14 15:26:08 -0300

    Exercício 5

---
+++ Lista/Ex5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n,d;
+            Console.Write("Digite um numero: ");
+            n = double.Parse(Console.ReadLine());
+            d = n * 2;
+            Console.WriteLine("O dobro de {0} é: {1} ", n, d);
+        }
+    }
+}
