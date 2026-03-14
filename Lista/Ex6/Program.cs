4d5d2f20e33f1277ac10146c55f0b0b9dc342a0c
Washingtoncip <washingtoncip@outlook.com>
2026-03-14 15:29:48 -0300

    Exercício 6

---
+++ Lista/Ex6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, d;
+            Console.Write("Digite um valor em USD$: ");
+            d = double.Parse(Console.ReadLine());
+            r = d * 5.33;
+            Console.WriteLine("Valor de USD$ {0} em R$: {1} ", d, r);
+        }
+    }
+}
