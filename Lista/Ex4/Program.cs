822a4d289bf2cfa5236a5162733a601f36459be4
Washingtoncip <washingtoncip@outlook.com>
2026-03-14 15:23:36 -0300

    Exercício 4

---
+++ Lista/Ex4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double C,F;
+            Console.Write("Digite a temperatura: Cº ");
+            C = double.Parse(Console.ReadLine());
+            F = C * 9/5 + 32;
+            Console.WriteLine("Temperatura em Fahrenheit: {0} ", F);
+        }
+    }
+}
